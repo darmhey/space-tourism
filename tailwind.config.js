b83603584d9b7abbaf91628eef0df9850b84c717
Damilola <johnasco19@gmail.com>
2023-11-14 07:43:17 +0100

    colors, fonts, assets,

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black': '#0b0d18',
+        'black-accent': '#45474f',
+        'white': '#ffffff',
+        'light': '#d0d6f9',
+        
+      },
+      fontFamily: {
+        'sans': ['Barlow', 'ui-sans-serif', 'system-ui'],
+        'sans-cond': ['Barlow Condensed', 'ui-sans-serif', 'system-ui'],
+        'serif': ['Bellefair', 'ui-serif', 'Georgia', ],
+        
+      }
+    },
   },
   plugins: [],
 }
