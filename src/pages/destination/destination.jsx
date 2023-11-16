e5d84e6855a34ab857707704919e457ec6564ece
Damilola <johnasco19@gmail.com>
2023-11-16 14:37:27 +0100

    destination page, interactive, not responsive

--- src/pages/destination/destination.jsx
+++ src/pages/destination/destination.jsx
@@ -1,18 +1,31 @@
 // import Navigation from "../../components/nav/Navigation"
-import moonImage from "../../../public/assets/destination/image-moon.png"
+//import moonImage from "../../../public/assets/destination/image-moon.png"
+import data from "../../../public/data/data"
+
+import { useState } from "react"
 
 function Destination () {
+
+
+  const [active, setActive] = useState(0)
+
+  
   return (
     <>
+ 
+      
       <div className=" px-6 bg-destination-mobile bg-cover bg-no-repeat
         md:bg-destination-tablet lg:bg-destination-desktop
       ">
         <div className=" h-16"></div>
-        {/* <div className=" bg-black mb-8 ">
-        <Navigation />
-        </div> */}
+          {/* <div className=" bg-black mb-8 ">
+              <Navigation />
+          </div> */}
         
         <div className="flex flex-col justify-center items-center space-y-6">
+
+          {/* --------title and image--------- */}
+        <div>
         <div className=" ">
           <h1 className=" md:text-20 md:tracking-3.38 lg:text-28 lg:tracking-4.72 
           text-white uppercase sans-cond text-16 tracking-2.7 leading-normal"> 
@@ -20,11 +33,14 @@ function Destination () {
           Pick your destination</h1>
         </div>
           <div className="md:w-3/5 w-3/5 mx-auto ">
-            <img src={moonImage} alt="planet moon" /> 
+            <img src={data.destinations[active].images.png} alt="planet moon" /> 
           </div> 
-
+        </div>
+       
           <div className="  text-12 tracking-2.7 leading-normal ">
-            <button aria-selected="true" 
+            <button 
+            onClick={() => setActive(0)}
+            aria-selected={active === 0 ? "true" : "false"} 
               className="uppercase pb-1 border-black-accent    aria-selected:text-white 
               aria-selected:border-white aria-selected:border-b-4 text-light  mr-4 hover:border-b-4
               md:text-16 md:tracking-2.7 
@@ -32,19 +48,25 @@ function Destination () {
               ">
                 Moon
             </button>
-            <button aria-selected="false" 
+            <button
+           onClick={() => setActive(1)}
+            aria-selected={active === 1 ? "true" : "false"}  
                className="uppercase pb-1 border-black-accent aria-selected:border-b-4 aria-selected:text-white 
                 aria-selected:border-white text-light hover:border-b-4 mr-4
                 md:text-16 md:tracking-2.7 
                 ">
                 Mars
             </button>
-            <button aria-selected="false" 
+            <button
+            onClick={() => setActive(2)}
+            aria-selected={active === 2 ? "true" : "false"}  
                 className="uppercase pb-1 aria-selected:text-white aria-selected:border-b-4
                   aria-selected:border-white border-black-accent text-light hover:border-b-4 mr-4 md:text-16 md:tracking-2.7 ">
                   Europa
             </button>
-            <button aria-selected="false" 
+            <button
+            onClick={() => setActive(3)}
+            aria-selected={active === 3 ? "true" : "false"}  
                 className="uppercase pb-1 aria-selected:text-white aria-selected:border-white
                 aria-selected:border-b-4 border-black-accent text-light hover:border-b-4 md:text-16 md:tracking-2.7 " >
                   Titan
@@ -54,7 +76,7 @@ function Destination () {
             md:text-80
             lg:text-100 
             text-56 font-serif uppercase">
-            <h1>Moon</h1>
+            <h1>{data.destinations[active].name}</h1>
           </div>
           <div>
             <p className="text-light text-center text-15 pb-8 leading-7
@@ -62,9 +84,7 @@ function Destination () {
              md:text-16 
              lg:text-18 
              ">
-                See our planet as you’ve never seen it before. A perfect relaxing trip away to help 
-                regain perspective and come back refreshed. While you’re there, take in some history 
-                by visiting the Luna 2 and Apollo 11 landing sites.
+               {data.destinations[active].description}
               </p>
           </div>
 
@@ -73,7 +93,7 @@ function Destination () {
               <h3 className=" text-14 tracking-2.36 leading-normal uppercase pb-2
                 md:text-14 md:tracking-2.36
               ">
-                  Avg. distance
+                  {data.destinations[active].distance}
               </h3>
               <p className=" text-white text-28 uppercase font-serif space-y-8 pb-4
                 md:text-28
@@ -86,7 +106,7 @@ function Destination () {
               <h3 className=" text-14 tracking-2.36 leading-normal uppercase pb-2
                 md:text-14 md:tracking-2.36
               ">
-                Est. travel time
+                {data.destinations[active].travel}
               </h3>
               <p className="  text-white text-28 uppercase font-serif md:text-28">
                 3 days
